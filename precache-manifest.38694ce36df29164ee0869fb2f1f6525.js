fc44f89521e2f3d912071e4ba34169aba90f6298
Nikita Holovnin <Golovninnikita@ukr.net>
2020-01-27 13:21:18 +0200

    Updates

---
+++ precache-manifest.38694ce36df29164ee0869fb2f1f6525.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df92399ff5fa07ab45ebe47176c1a0a4",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "611eece894e5ca209203",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.f11684b3.chunk.css"
+  },
+  {
+    "revision": "490e56de31dbdacfbacf",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ccb782f5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ccb782f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "611eece894e5ca209203",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9312b72c.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "8c608256fb0273e3a36e6b603f71f213",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-500.8c608256.ttf"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/goit-react-hw-02-phonebook/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
